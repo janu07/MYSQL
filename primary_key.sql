c93a8b7b12f7125205dd99d553a67e117952b00c
janu07 <47377091+janu07@users.noreply.github.com>
2019-03-09 15:46:18 +0530

    Create primary_key.sql

---
+++ primary_key.sql
@@ -0,0 +1,28 @@
+create database company;
+show databases;
+use company;
+
+create table Employee(
+    Name VARCHAR(30) NOT NULL,
+    Emp_Id VARCHAR(30) NOT NULL,
+    Sex VARCHAR(30) NOT NULL,
+    Dept VARCHAR(30) NOT NULL,
+    Year VARCHAR(30) NOT NULL,
+    ContactNO VARCHAR(20) NOT NULL,
+    Email_Id VARCHAR(30) NOT NULL,
+    City VARCHAR(30) NOT NULL,
+    State VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Emp_Id)
+    );
+    
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('Janu','111','F','IT','3rd','2345675678','janu@gmail.com','Erode','TamilNadu');
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('Sharmi','112','F','IT','3rd','2345675678','fvhgdj@gmail.com','selam','TamilNadu');
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('Tamil','113','M','IT','3rd','4647392113','tamil@gmail.com','chennai','TamilNadu');
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('deepak','114','M','IT','3rd','2343475678','mnbvkzCKn@gmail.com','Erode','TamilNadu');
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('Sathish','115','M','CSE','3rd','2378975678','jgsdfert@gmail.com','chennai','TamilNadu');
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('Renu','116','F','CSE','3rd','2564375678','kjhiknbj@gmail.com','thirupur','TamilNadu');
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('Mathan','117','M','IT','3rd','23453275678','janu]bffdf@gmail.com','Erode','TamilNadu');
+    INSERT into Employee(Name,Emp_Id,Sex,Dept,Year,ContactNO,Email_Id,City,State)VALUES('Sheela','119','F','MECH','3rd','2348987678','jandfhgfdhgu@gmail.com','thirupur','TamilNadu');
+    
+    SELECT * FROM Employee;
+    
